88846f370fc6a01f48f2c876a6f82637999ee8ed
Nick Webster <nick@nick.geek.nz>
2016-11-10 19:51:00 +1300

    Fixing issue where a required field was missing

--- src/js/parse.js
+++ src/js/parse.js
@@ -34,49 +34,76 @@ Note.prototype.addSource = function(id, item, contents) {
 		contents: contents
 	});
 };
+Note.prototype.addElement = function(args, content) {
+	this.elements.push({
+		args: args,
+		content: content
+	});
+};
 
 exports.parse = function parse(xml) {
 	parseString(xml, function(e, res) {
 		var notepad = new Notepad(res.notepad.$.title);
-		// console.log(res.notepad.section);
 		for (var i = 0; i < res.notepad.section.length; i++) {
 			var sectionXML = res.notepad.section[i];
 			var section = new Section(sectionXML.$.title);
-			// console.log(sectionXML.note[0].addons[0].import[0]);
 
-			for (var k in sectionXML){
-				if (typeof sectionXML[k] !== 'function') {
-					var v = sectionXML[k];
-
-					switch (k) {
-						case "note":
-							for (var i = 0; i < v.length; i++) {
-								var noteXML = v[i];
-
-								var title = noteXML.$.title;
-								var time = noteXML.$.time;
-								var addons = [];
-
-								if (noteXML.addons[0].import) {
-									for (var j = noteXML.addons[0].import.length - 1; j >= 0; j--) {
-										var addon = noteXML.addons[0].import[j];
-										addons.push(addon);
-									}
-								}
-
-								var note = new Note(title, time, addons);
-
-								if (noteXML.bibliography[0].source) {
-									for (var l = noteXML.bibliography[0].source.length - 1; l >= 0; l--) {
-										var source = noteXML.bibliography[0].source[l];
-										note.addSource(source.$.id, source.$.item, source._);
-									}
-								}
-							}
-							break;
-					}
-				}
-			}
+			parseSection(sectionXML, section, notepad);
 		}
 	});
+}
+
+function parseSection(sectionXML, section, parent) {
+	for (var k in sectionXML) {
+		if (typeof sectionXML[k] !== 'function') {
+			var v = sectionXML[k];
+
+			switch (k) {
+				case "note":
+					for (var i = 0; i < v.length; i++) {
+						var noteXML = v[i];
+
+						var title = noteXML.$.title;
+						var time = noteXML.$.time;
+						var addons = [];
+
+						if (noteXML.addons[0].import) {
+							for (var j = noteXML.addons[0].import.length - 1; j >= 0; j--) {
+								var addon = noteXML.addons[0].import[j];
+								if (!isCompatible(addon)) console.log("Some features of this document are not supported");
+								addons.push(addon);
+							}
+						}
+
+						var note = new Note(title, time, addons);
+
+						if (noteXML.bibliography[0].source) {
+							for (var l = noteXML.bibliography[0].source.length - 1; l >= 0; l--) {
+								var source = noteXML.bibliography[0].source[l];
+								note.addSource(source.$.id, source.$.item, source._);
+							}
+						}
+
+						if (noteXML.markdown) {
+							for (var i = 0; i < noteXML.markdown.length; i++) {
+								var element = noteXML.markdown[i];
+								note.addElement(element.$, element._);
+							}
+						}
+					}
+					break;
+
+				case "section":
+					// console.log(v[0].$.title);
+					parseSection(v[0], new Section(v[0].$.title), section);
+					// console.log('-----------------')
+					break;
+			}
+		}
+	}
+}
+
+function isCompatible(addonName) {
+	if (addonName == "asciimath") return true;
+	return false;
 }
