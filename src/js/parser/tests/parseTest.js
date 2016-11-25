a21af0c7df276013b09b2fdd3af741c22b7fc17d
Nick Webster <nick@nick.geek.nz>
2016-11-25 15:46:23 +1300

    Updating test

--- src/js/parser/tests/parseTest.js
+++ src/js/parser/tests/parseTest.js
@@ -2,7 +2,7 @@ var parser = require('../parse.js');
 var fs = require('fs');
 var path = require('path');
 
-fs.readFile(path.resolve('../../../examples/test.npx'), 'utf-8', function(e, data) {
+fs.readFile(path.resolve('../../../examples/Example Notepad.npx'), 'utf-8', function(e, data) {
 	parser.parse(data, ["asciimath"]);
 	while (!parser.notepad) if (parser.notepad) break;
 	console.log(parser.notepad);
