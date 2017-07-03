af74ecb2f50e0531f2f91e95e377ca86c05c26af
Nick Webster <nick@nick.geek.nz>
2017-07-03 16:21:29 +1200

    Integrating Framework7 and PhoneGap

--- src/ios/cordova/www/js/index.js
+++ src/ios/cordova/www/js/index.js
@@ -17,30 +17,73 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+	// Application Constructor
+	initialize: function() {
+		document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+	},
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+	// deviceready Event Handler
+	//
+	// Bind any cordova events here. Common events are:
+	// 'pause', 'resume', etc.
+	onDeviceReady: function() {
+		this.receivedEvent('deviceready');
+	},
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+	// Update DOM on a Received Event
+	receivedEvent: function(id) {
+		console.log('Received Event: ' + id);
+	}
 };
 
 app.initialize();
+
+// Initialize your app
+var myApp = new Framework7({
+	init: false
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+	// Because we use fixed-through navbar we can enable dynamic navbar
+	dynamicNavbar: true
+});
+
+// Callbacks to run specific code for specific pages, for example for About page:
+myApp.onPageInit('index', function(page) {
+	
+});
+
+myApp.init();
+
+// Generate dynamic page
+var dynamicPageIndex = 0;
+function createContentPage() {
+	mainView.router.loadContent(
+		'<!-- Top Navbar-->' +
+		'<div class="navbar">' +
+		'  <div class="navbar-inner">' +
+		'    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
+		'    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
+		'  </div>' +
+		'</div>' +
+		'<div class="pages">' +
+		'  <!-- Page, data-page contains page name-->' +
+		'  <div data-page="dynamic-pages" class="page">' +
+		'    <!-- Scrollable page content-->' +
+		'    <div class="page-content">' +
+		'      <div class="content-block">' +
+		'        <div class="content-block-inner">' +
+		'          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
+		'          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
+		'        </div>' +
+		'      </div>' +
+		'    </div>' +
+		'  </div>' +
+		'</div>'
+	);
+	return;
+}
