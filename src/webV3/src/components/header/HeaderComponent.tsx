6019694b1d562f2a979372c5f6d7682bf2ee764f
Nick Webster <nick@nick.geek.nz>
2018-02-08 23:52:14 +1300

    Notepad dropdown (this could probably be its own component)

--- src/webV3/src/components/header/HeaderComponent.tsx
+++ src/webV3/src/components/header/HeaderComponent.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import AppNameComponent from '../../containers/header/AppNameContainer';
 // @ts-ignore
-import { Icon, Navbar, NavItem } from 'react-materialize';
+import { Icon, Navbar, NavItem, Dropdown } from 'react-materialize';
+import { SYNC_NAME } from '../../types';
 
 export default class HeaderComponent extends React.Component {
 	private readonly navStyle = {
@@ -11,7 +12,21 @@ export default class HeaderComponent extends React.Component {
 	render() {
 		return (
 			<Navbar className="blue-grey" brand={<AppNameComponent />} style={this.navStyle} right={true}>
-				<NavItem><Icon left={true}>file_upload</Icon> Upload Notepad</NavItem>
+				<li>
+					<Dropdown trigger={
+						<ul>
+							<NavItem>
+								<Icon left={true}>collections_bookmark</Icon> Notepads <Icon right={true}>arrow_drop_down</Icon>
+							</NavItem>
+						</ul>
+					}>
+						<NavItem><Icon left={true}>add</Icon> New</NavItem>
+						<NavItem><Icon left={true}>cloud_download</Icon> Open ({SYNC_NAME})</NavItem>
+						<NavItem><Icon left={true}>file_upload</Icon> Upload</NavItem>
+						<NavItem><Icon left={true}>file_download</Icon> Export All</NavItem>
+					</Dropdown>
+				</li>
+
 				<NavItem><Icon left={true}>search</Icon> Search</NavItem>
 				<NavItem><Icon left={true}>help_outline</Icon> Help</NavItem>
 			</Navbar>
