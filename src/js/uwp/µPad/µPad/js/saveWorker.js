3caf348937967d0159fa668d12119fc7eab73f5f
Nick Webster <nick@nick.geek.nz>
2018-02-08 23:56:58 +1300

    Removing some redundant JS

--- src/js/uwp/µPad/µPad/js/saveWorker.js
+++
@@ -1,6 +0,0 @@
-﻿/// <reference group="Dedicated Worker" />
-importScripts('parser.js');
-
-onmessage = function(event) {
-	postMessage({ xml: new Blob([parser.xmlObjToXML(event.data.xmlObj)], { type: "text/xml;charset=utf-8" }), fileLoad: event.data.fileLoad, bulk: event.data.bulk });
-}
