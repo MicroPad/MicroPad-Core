f43aba1fd0edab7d541e5ce4b8bb4c9f025d8ba6
Nick Webster <nick@nick.geek.nz>
2018-02-09 22:21:10 +1300

    Moving the notepad dropdown to its own component

--- src/webV3/src/components/header/HeaderComponent.tsx
+++ src/webV3/src/components/header/HeaderComponent.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import AppNameComponent from '../../containers/header/AppNameContainer';
 // @ts-ignore
-import { Icon, Navbar, NavItem, Dropdown } from 'react-materialize';
-import { SYNC_NAME } from '../../types';
+import { Dropdown, Icon, Navbar, NavItem } from 'react-materialize';
+import NotepadDropdownComponent from './AppNameComponent/NotepadDropdownComponent';
 
 export default class HeaderComponent extends React.Component {
 	private readonly navStyle = {
@@ -12,21 +12,7 @@ export default class HeaderComponent extends React.Component {
 	render() {
 		return (
 			<Navbar className="blue-grey" brand={<AppNameComponent />} style={this.navStyle} right={true}>
-				<li>
-					<Dropdown trigger={
-						<ul>
-							<NavItem>
-								<Icon left={true}>collections_bookmark</Icon> Notepads <Icon right={true}>arrow_drop_down</Icon>
-							</NavItem>
-						</ul>
-					}>
-						<NavItem><Icon left={true}>add</Icon> New</NavItem>
-						<NavItem><Icon left={true}>cloud_download</Icon> Open ({SYNC_NAME})</NavItem>
-						<NavItem><Icon left={true}>file_upload</Icon> Upload</NavItem>
-						<NavItem><Icon left={true}>file_download</Icon> Export All</NavItem>
-					</Dropdown>
-				</li>
-
+				<NotepadDropdownComponent />
 				<NavItem><Icon left={true}>search</Icon> Search</NavItem>
 				<NavItem><Icon left={true}>help_outline</Icon> Help</NavItem>
 			</Navbar>
