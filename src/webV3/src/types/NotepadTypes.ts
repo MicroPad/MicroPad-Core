f5705708a8261104dd3cbb079b8e5bbf5b2c3ded
Nick Webster <nick@nick.geek.nz>
2018-02-19 17:29:59 +1300

    Adding interfaces and types for notepads and related structures

--- src/webV3/src/types/NotepadTypes.ts
+++ src/webV3/src/types/NotepadTypes.ts
@@ -1,56 +1,101 @@
+export type ToMarkdownCallback = (markdownNotes: IMarkdownNote[]) => void;
+
+export type ToXMLCallback = (xml: string) => void;
+
+export type ToXMLObjectCallback = (xmlObject: object) => void;
+
+export type Source = {
+	id: string;
+	item: string;
+	content: string;
+};
+
+export type Element = {
+	type: string;
+	content: string;
+	args: IElementArgs;
+};
+
+export interface IElementArgs {
+	id: string;
+	x: string;
+	y: string;
+	width?: string;
+	height?: string;
+	fontSize?: string;
+	filename?: string;
+	ext?: string;
+}
+
 export interface INotepadStoreState {
 	savedNotepadTitles: string[];
-	notepads: Notepad[];
+	notepads: INotepad[];
 }
 
-export interface NPXObject {
+export interface INPXObject {
 	title: string;
-	toXML: (toXMLCallback, Assets) => void;
-	toXMLObject: (toXMLObjectCallback) => void;
+	toXML: (callback: ToXMLCallback, assets: IAssets) => void;
+	toXMLObject: (callback: ToXMLObjectCallback) => void;
 }
 
-export interface Parent extends NPXObject {
-	sections: Section[];
+export interface IParent extends INPXObject {
+	sections: ISection[];
 
-	search: (string) => Note[];
-	addSection: (Section) => void;
+	search: (query: string) => INote[];
+	addSection: (section: ISection) => void;
 }
 
-export interface Notepad extends Parent {
-	assets: Assets;
+export interface INotepad extends IParent {
+	assets: IAssets;
 	notepadAssets: Set<string>;
 	lastModified: string;
 
 	getUsedAssets: () => Set<string>;
-	toMarkdown: (ToMarkdownCallback, Assets) => void;
+	toMarkdown: (callback: ToMarkdownCallback, assets: IAssets) => void;
 }
 
-export interface Section extends Parent {
-	parent: Parent;
-	notes: Note[];
+export interface ISection extends IParent {
+	parent: IParent;
+	notes: INote[];
 
-	addNote: (Note) => void;
+	addNote: (note: INote) => void;
 	getUsedAssets: () => string[];
-	toMarkdown: (object) => MarkdownNote[];
+	toMarkdown: (b64assets: object) => IMarkdownNote[];
 }
 
-export interface Note extends NPXObject {
+export interface INote extends INPXObject {
+	addons: string[];
+	bibliography: Source[];
+	elements: Element[];
+	parent: IParent;
+	time: number;
+
+	addSource: (id: string, item: string, content: string) => void;
+	addElement: (type: string, args: IElementArgs, content: string) => void;
+	search: (query: string) => INote;
+	toXML: () => string;
+	toXMLObject: () => object;
+	toMarkdown: (b64assets: object) => IMarkdownNote;
 	getUsedAssets: () => string[];
 }
 
-export interface MarkdownNote {
+export interface IMarkdownNote {
 	title: string;
 	md: string;
 }
 
-export interface Assets {
-	assets: Asset[];
+export interface IAssets {
+	assets: IAsset[];
+
+	addAsset: (asset: IAsset) => void;
+	getXmlObject: (callback: ToXMLObjectCallback) => void;
+	getBase64Assets: (callback: Function) => void;
 }
 
-export interface Asset {
+export interface IAsset {
+	uuid: string;
+	data: Blob;
 
+	toString: (callback: Function) => void;
+	getXmlObject: (callback: ToXMLObjectCallback) => void;
 }
-
-export type ToMarkdownCallback = (markdownNotes: MarkdownNote[]) => void;
-export type toXMLCallback = (xml: string) => void;
-export type toXMLObjectCallback = (xmlObject: object) => void;
