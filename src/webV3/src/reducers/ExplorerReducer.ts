7ccf2c0a1ecfe1e3fb9edc6a456a1bef7c5a5351
Nick Webster <nick@nick.geek.nz>
2018-03-04 15:57:17 +1300

    Auto-expand to open note

--- src/webV3/src/reducers/ExplorerReducer.ts
+++ src/webV3/src/reducers/ExplorerReducer.ts
@@ -2,6 +2,7 @@ import { IReducer } from '../types/ReducerType';
 import { Action } from 'redux';
 import { isType } from 'redux-typescript-actions';
 import { actions } from '../actions';
+import { INote, ISection } from '../types/NotepadTypes';
 
 export interface IExplorerState {
 	openSections: string[];
@@ -14,7 +15,9 @@ export class ExplorerReducer implements IReducer<IExplorerState> {
 	};
 
 	public reducer(state: IExplorerState, action: Action): IExplorerState {
-		if (isType(action, actions.expandSection)) {
+		if (isType(action, actions.parseNpx.done) || isType(action, actions.parseNpx.failed) || isType(action, actions.deleteNotepad)) {
+			return this.initialState;
+		} else if (isType(action, actions.expandSection)) {
 			const guid: string = action.payload;
 
 			return {
@@ -28,6 +31,20 @@ export class ExplorerReducer implements IReducer<IExplorerState> {
 
 			return {
 				openSections: state.openSections.filter((guid: string) => guid !== guidToClose)
+			};
+		} else if (isType(action, actions.loadNote)) {
+			const note: INote = action.payload;
+			const noteFamily: Set<string> = new Set<string>(state.openSections);
+
+			// Add the note and its parents to the list
+			let parent: ISection = <ISection> note.parent;
+			while (!!parent.parent) {
+				noteFamily.add(parent.internalRef);
+				parent = <ISection> parent.parent;
+			}
+
+			return {
+				openSections: Array.from(noteFamily)
 			};
 		}
 
