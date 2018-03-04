0f81c1ab28ce214431b9dee3f3d81549a4ce5fcd
Nick Webster <nick@nick.geek.nz>
2018-03-04 18:25:22 +1300

    Search by title

--- src/webV3/src/components/search/SearchComponent.tsx
+++ src/webV3/src/components/search/SearchComponent.tsx
@@ -1,17 +1,26 @@
 import * as React from 'react';
 import { Icon, Autocomplete, Modal, NavItem, Row, Collection, CollectionItem } from 'react-materialize';
 import { INote, INotepad } from '../../types/NotepadTypes';
+import * as ReactDOM from 'react-dom';
 
 export interface ISearchComponentProps {
 	notepad: INotepad;
+	loadNote?: (note: INote) => void;
 }
 
 export default class SearchComponent extends React.Component<ISearchComponentProps> {
+	private searchInput: HTMLInputElement;
+	private mappedNotesToOptions: INote[];
+
 	render() {
 		const { notepad } = this.props;
 
 		const autoCompleteOptions = {};
-		notepad.search('').map((note: INote) => autoCompleteOptions[note.title] = null);
+		this.mappedNotesToOptions = [];
+		notepad.search('').forEach((note: INote, i: number) => {
+			autoCompleteOptions[`${i}. ${note.title}`] = null;
+			this.mappedNotesToOptions[i] = note;
+		});
 
 		return (
 			<Modal
@@ -19,9 +28,20 @@ export default class SearchComponent extends React.Component<ISearchComponentPro
 				header="Search Notepad"
 				trigger={<NavItem href="#!"><Icon left={true}>search</Icon> Search</NavItem>}>
 				<Row>
-					<Autocomplete s={12} title="Search by note title" data={autoCompleteOptions} />
+					<Autocomplete
+						s={12}
+						title="Search by note title"
+						onAutocomplete={this.loadNoteFromInput}
+						data={autoCompleteOptions} />
 				</Row>
 			</Modal>
 		);
+	}
+
+	private loadNoteFromInput = (value: string) => {
+		const { loadNote } = this.props;
+		const index: number = parseInt(value.split('.')[0], 10);
+
+		loadNote!(this.mappedNotesToOptions[index]);
 	}
 }
--- src/webV3/src/containers/SearchContainer.ts
+++ src/webV3/src/containers/SearchContainer.ts
@@ -1,8 +1,9 @@
 import { IStoreState } from '../types';
 import { connect, Dispatch } from 'react-redux';
-import { INotepadsStoreState, INotepadStoreState } from '../types/NotepadTypes';
+import { INote, INotepadsStoreState, INotepadStoreState } from '../types/NotepadTypes';
 import { Action } from 'redux';
 import { ISearchComponentProps, default as SearchComponent } from '../components/search/SearchComponent';
+import { actions } from '../actions';
 
 export function mapStateToProps({ notepads }: IStoreState) {
 	return {
@@ -11,7 +12,9 @@ export function mapStateToProps({ notepads }: IStoreState) {
 }
 
 export function mapDispatchToProps(dispatch: Dispatch<Action>): Partial<ISearchComponentProps> {
-	return {};
+	return {
+		loadNote: (note: INote) => dispatch(actions.loadNote(note))
+	};
 }
 
 export default connect<ISearchComponentProps>(mapStateToProps, mapDispatchToProps)(SearchComponent);
