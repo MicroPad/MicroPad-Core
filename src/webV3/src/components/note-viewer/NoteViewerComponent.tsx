31e96f3320e956400f6750bcb87b599304c0b2eb
Nick Webster <nick@nick.geek.nz>
2018-03-10 20:28:36 +1300

    Fixing more issues

--- src/webV3/src/components/note-viewer/NoteViewerComponent.tsx
+++ src/webV3/src/components/note-viewer/NoteViewerComponent.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import './NoteViewerComponent.css';
 import { INote } from '../../types/NotepadTypes';
 import NoteElementComponent from './elements/NoteElementComponent';
-import { generateGuid } from 'src/util';
 import * as Materialize from 'materialize-css/dist/js/materialize.js';
+import * as md5 from 'md5';
 
 export interface INoteViewerComponentProps {
 	isFullscreen: boolean;
@@ -31,7 +31,7 @@ export default class NoteViewerComponent extends React.Component<INoteViewerComp
 
 		const elements: JSX.Element[] = [];
 		if (!!note) note.elements.forEach(element => elements.push(
-			<NoteElementComponent key={element.args.id} element={element} noteAssets={noteAssets} />
+			<NoteElementComponent key={md5(JSON.stringify(element))} element={element} noteAssets={noteAssets} />
 		));
 
 		if (!!note && elements.length === 0) Materialize.toast('Welcome to your note! Press anywhere on here to insert an element.', 3000);
