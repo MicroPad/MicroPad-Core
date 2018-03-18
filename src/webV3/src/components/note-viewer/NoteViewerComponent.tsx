a9b9abc9ce7090fe8925e3c4f6b929fe417b491a
Nick Webster <nick@nick.geek.nz>
2018-03-18 22:01:36 +1300

    Added some rudimentary zoom code that might be introduced at some point

--- src/webV3/src/components/note-viewer/NoteViewerComponent.tsx
+++ src/webV3/src/components/note-viewer/NoteViewerComponent.tsx
@@ -14,6 +14,9 @@ export interface INoteViewerComponentProps {
 }
 
 export default class NoteViewerComponent extends React.Component<INoteViewerComponentProps> {
+	private viewerDiv: HTMLDivElement;
+	private lastZoom: number | null;
+
 	render() {
 		const { isFullscreen, note, noteAssets, search, downloadAsset } = this.props;
 
@@ -40,10 +43,52 @@ export default class NoteViewerComponent extends React.Component<INoteViewerComp
 
 		return (
 			<div id="note-viewer" className={classes} style={styles}>
-				<div id="note-container" style={containerStyles}>
+				<div id="note-container" style={containerStyles} ref={div => this.viewerDiv = div!}>
 					{elements}
 				</div>
 			</div>
 		);
 	}
+
+	// componentDidUpdate() {
+	// 	if (!!this.props.note) {
+	// 		this.viewerDiv.ontouchmove = this.handleScale;
+	// 		this.viewerDiv.ontouchend = () => this.lastZoom = null;
+	// 	}
+	// }
+	//
+	// componentWillUnmount() {
+	// 	if (!!this.props.note) this.viewerDiv.removeEventListener('touchmove', this.handleScale);
+	// }
+	//
+	// private handleScale = (event: TouchEvent) => {
+	// 	if (event.touches.length !== 2) return;
+	//
+	// 	const factor = Math.hypot(
+	// 		event.touches[0].pageX - event.touches[1].pageX,
+	// 		event.touches[0].pageY - event.touches[1].pageY
+	// 	);
+	//
+	// 	if (!this.lastZoom) {
+	// 		this.lastZoom = factor;
+	// 		return;
+	// 	}
+	//
+	// 	const oldScale: number = parseFloat(this.viewerDiv.style.transform!.split('scale(').pop()!.slice(0, -1) || '1');
+	// 	console.log(oldScale);
+	// 	const scaleFactor = (0.002 * Math.abs(factor - this.lastZoom));
+	//
+	// 	if (factor > this.lastZoom) {
+	// 		// Zoom in
+	// 		console.log('zoom in');
+	// 		this.viewerDiv.style.transform = `scale(${oldScale + scaleFactor})`;
+	// 	} else {
+	// 		// Zoom out
+	// 		console.log('zoom out');
+	// 		this.viewerDiv.style.transform = `scale(${oldScale - scaleFactor})`;
+	// 	}
+	//
+	// 	console.log(factor);
+	// 	this.lastZoom = null;
+	// }
 }
