e252fe6c715bc26387839774ab8342c43818098a
Nick Webster <nick@nick.geek.nz>
2018-04-30 23:31:15 +1200

    Auto-expand new sections (will conflict with duplicate names with the same parent)

--- src/webV3/src/epics/ExplorerEpics.ts
+++ src/webV3/src/epics/ExplorerEpics.ts
@@ -2,7 +2,10 @@ import { combineEpics } from 'redux-observable';
 import { filter, map } from 'rxjs/operators';
 import { Action, isType } from 'redux-typescript-actions';
 import { actions } from '../actions';
-import { INotepad, INotepadStoreState, ISection } from '../types/NotepadTypes';
+import { INote, INotepad, INotepadStoreState, ISection } from '../types/NotepadTypes';
+import { getNotepadObjectByRef, isAction } from '../util';
+import { INewNotepadObjectAction } from '../types/ActionTypes';
+import { IStoreState } from '../types';
 
 const expandAll$ = (action$, store) =>
 	action$.pipe(
@@ -24,6 +27,25 @@ const expandAll$ = (action$, store) =>
 		map((allRefs: string[]) => actions.expandAllExplorer.done({ params: undefined, result: allRefs }))
 	);
 
+const createSection$ = (action$, store) =>
+	action$.pipe(
+		isAction(actions.newSection),
+		map((action: Action<INewNotepadObjectAction>) => [action.payload, (<IStoreState> store.getState()).notepads.notepad!.item]),
+		filter(([insertAction, notepad]: [INewNotepadObjectAction, INotepad]) => !!insertAction && !!insertAction.parent.internalRef && !!notepad),
+		map(([insertAction, notepad]: [INewNotepadObjectAction, INotepad]) => {
+			// Find the new section
+			let parent: ISection | false = false;
+			getNotepadObjectByRef(notepad, insertAction.parent.internalRef!, obj => parent = <ISection> obj);
+
+			return [insertAction, parent];
+		}),
+		filter(([insertAction, parent]: [INewNotepadObjectAction, ISection]) => !!parent),
+		map(([insertAction, parent]: [INewNotepadObjectAction, ISection]) => parent.sections.find(s => s.title === insertAction.title)),
+		filter(Boolean),
+		map((newSection: ISection) => actions.expandSection(newSection.internalRef))
+	);
+
 export const explorerEpics$ = combineEpics(
-	expandAll$
+	expandAll$,
+	createSection$
 );
