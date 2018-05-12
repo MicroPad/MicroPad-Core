7282d0fe0b5f6c6f5dfb5ffef6d236eb891b6cfe
Nick Webster <nick@nick.geek.nz>
2018-05-12 16:35:11 +1200

    Updating parser and adding a close button to the editors

--- src/webV3/src/components/note-viewer/elements/NoteElementComponent.tsx
+++ src/webV3/src/components/note-viewer/elements/NoteElementComponent.tsx
@@ -116,10 +116,16 @@ export default class NoteElementComponent extends React.Component<INoteElementCo
 
 						{
 							!!elementComponent && isEditing &&
-							<Row style={{paddingLeft: '5px'}}>
-								<Button className="red" waves="light" onClick={this.delete} style={{marginRight: '5px'}}><Icon left={true}>delete_forever</Icon> Delete</Button>
-								<span style={{marginRight: '5px'}}><SourcesComponent /></span>
-							</Row>
+							<div>
+								<Row style={{paddingLeft: '5px'}}>
+									<Button className="red" waves="light" onClick={this.delete} style={{marginRight: '5px'}}><Icon left={true}>delete_forever</Icon> Delete</Button>
+									<span style={{marginRight: '5px'}}><SourcesComponent /></span>
+								</Row>
+
+								<Row style={{paddingLeft: '5px'}}>
+									<Button className="btn-flat" waves="light" onClick={() => edit('')} style={{marginRight: '5px'}}>Close</Button>
+								</Row>
+							</div>
 						}
 					</div>
 				</div>
