794a9a5881ed85ac57d710b0ada03eb9f5351fef
Nick Webster <nick@nick.geek.nz>
2018-05-14 14:56:23 +1200

    Renaming folder

--- src/src/components/note-viewer/elements/markdown/enable-tabs.js
+++
@@ -1,3 +0,0 @@
-export function enableTabs(event) {
-	if(event.keyCode===9){var v=this.value,s=this.selectionStart,e=this.selectionEnd;this.value=v.substring(0, s)+'\t'+v.substring(e);this.selectionStart=this.selectionEnd=s+1;return false;}
-}
--- src/src/types/ReducerType.ts
+++
@@ -1,7 +0,0 @@
-import { Reducer } from 'redux';
-
-export interface IReducer<T> {
-	readonly key: string;
-	readonly initialState: T;
-	reducer: Reducer<T>;
-}
