98fbcff12e66d55d4d873b18618e76d1f0a8c451
Nick Webster <nick@nick.geek.nz>
2018-05-15 21:31:39 +1200

    Added some feature detection to alert unsupported browsers

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -5,7 +5,7 @@ import 'materialize-css/dist/js/materialize.js';
 import * as React from 'react';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import { IStoreState, MICROPAD_URL } from './types';
+import { APP_NAME, IStoreState, MICROPAD_URL } from './types';
 import { applyMiddleware, createStore } from 'redux';
 import { BaseReducer } from './reducers/BaseReducer';
 import { epicMiddleware } from './epics';
@@ -47,6 +47,13 @@ export const ASSET_STORAGE = localforage.createInstance({
 
 Promise.all([NOTEPAD_STORAGE.ready(), ASSET_STORAGE.ready(), localforage.getItem('font size')])
 	.then(([r, r2, fs]: [void, void, string]) => !!fs && store.dispatch(actions.updateDefaultFontSize(fs)))
+	.then(() => new Promise((resolve, reject) => {
+		if (doesSupportSrcDoc()) {
+			resolve();
+		} else {
+			reject();
+		}
+	}))
 	.then(() => store.dispatch(actions.getNotepadList.started(undefined)))
 	.then(() => ReactDOM.render(
 		<Provider store={store}>
@@ -76,8 +83,27 @@ Promise.all([NOTEPAD_STORAGE.ready(), ASSET_STORAGE.ready(), localforage.getItem
 	.then(() => {
 		const downloadNotepadUrl = QueryString.parse(location.search.slice(1)).download;
 		if (!!downloadNotepadUrl && typeof downloadNotepadUrl === 'string') store.dispatch(actions.downloadNotepad.started(downloadNotepadUrl));
-	});
+	})
+	.catch(() => ReactDOM.render(
+		<div style={{ margin: '10px' }}>
+			<h1>Bad news 😢</h1>
+			<p>
+				Your web-browser doesn't support important security features required for {APP_NAME} v{store.getState().meta.version.major} to function.<br />
+				Try with a more modern browser like <a href="https://www.google.com/chrome/" target="_blank" rel="nofollow noreferrer">Google Chrome</a> or <a href="https://www.mozilla.org/firefox/" target="_blank" rel="nofollow noreferrer">Mozilla Firefox</a>.
+			</p>
+			<p>
+				You can always use the old {APP_NAME} <a href="https://getmicropad.com/web">here</a>.
+			</p>
+		</div>,
+		document.getElementById('react-no-print') as HTMLElement
+	));
 
 new OldSyncHandler(store);
 enableKeyboardShortcuts(store);
 registerServiceWorker();
+
+function doesSupportSrcDoc(): boolean {
+	const testIframe = document.createElement('iframe');
+	testIframe.srcdoc = 'test';
+	return testIframe.getAttribute('srcdoc') === 'test';
+}
