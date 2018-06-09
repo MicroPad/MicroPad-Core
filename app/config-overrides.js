10fc3a229bbfba91e61666b4758eb7c292c2855d
Nick Webster <nick@nick.geek.nz>
2018-06-09 21:11:12 +1200

    Ignoring the webworker on the fallback

--- app/config-overrides.js
+++ app/config-overrides.js
@@ -1,3 +1,8 @@
 module.exports = function override(config, env) {
+	if (env === 'production') {
+		let swPlugin = config.plugins.find(p => p.constructor.name === 'SWPrecacheWebpackPlugin');
+		swPlugin.options.navigateFallbackWhitelist.push(/^(?!\/[a-zA-Z0-9]*\.worker).*/);
+	}
+
 	return config;
 };
