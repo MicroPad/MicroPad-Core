32a2414162ab8ad97687cccf80e94f1b97aaac0e
Nick Webster <nick@nick.geek.nz>
2018-06-09 17:12:53 +1200

    Update price

--- app/src/components/sync/LoginComponent.tsx
+++ app/src/components/sync/LoginComponent.tsx
@@ -70,7 +70,7 @@ export default class LoginComponent extends React.Component<ILoginComponentProps
 						</p>
 						<p>
 							It's completely free for any notepad that's mostly text,
-							and only NZ$3/mth for up to twenty of your more advanced
+							and only NZ$2.99/m for up to twenty of your more advanced
 							notepads. <a target="_blank" href={`${MICROPAD_URL}/sync/pricing`}>More information >></a>
 						</p>
 					</div>
