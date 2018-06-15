cd908242d1d165757e3487d9690b0e601c9ef4fe
Nick Webster <nick@nick.geek.nz>
2018-06-15 16:35:16 +1200

    Updating AppNameComponent tests

--- app/src/components/header/AppNameComponent/AppNameComponent.spec.tsx
+++ app/src/components/header/AppNameComponent/AppNameComponent.spec.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as enzyme from 'enzyme';
 import AppNameComponent from './AppNameComponent';
+import { APP_NAME } from '../../../types';
 
 describe(`version number tests`, () => {
 	const statusToSymbol = {
@@ -10,39 +11,19 @@ describe(`version number tests`, () => {
 		stable: ''
 	};
 
-	it('should display correctly for stable status', () => {
-		// Arrange
-		const component = enzyme.shallow(<AppNameComponent major={1} minor={2} patch={3} status={'stable'}/>);
+	[
+		'stable',
+		'beta',
+		'alpha',
+		'dev'
+	].forEach(status => {
+		it(`should display correctly for ${status} status`, () => {
+			// Arrange
+			const component = enzyme.shallow(<AppNameComponent major={1} minor={2} patch={3} status={status as any}/>);
 
-		// Assert
-		expect(component.find('.AppNameComponent__version').text()).toEqual(statusToSymbol.stable);
-		expect(component.find('.AppNameComponent__version').props().title).toEqual('v1.2.3-stable');
-	});
-
-	it('should display correctly for beta status', () => {
-		// Arrange
-		const component = enzyme.shallow(<AppNameComponent major={1} minor={2} patch={3} status={'beta'}/>);
-
-		// Assert
-		expect(component.find('.AppNameComponent__version').text()).toEqual(statusToSymbol.beta);
-		expect(component.find('.AppNameComponent__version').props().title).toEqual('v1.2.3-beta');
-	});
-
-	it('should display correctly for alpha status', () => {
-		// Arrange
-		const component = enzyme.shallow(<AppNameComponent major={1} minor={2} patch={3} status={'alpha'}/>);
-
-		// Assert
-		expect(component.find('.AppNameComponent__version').text()).toEqual(statusToSymbol.alpha);
-		expect(component.find('.AppNameComponent__version').props().title).toEqual('v1.2.3-alpha');
-	});
-
-	it('should display correctly for dev status', () => {
-		// Arrange
-		const component = enzyme.shallow(<AppNameComponent major={1} minor={2} patch={3} status={'dev'}/>);
-
-		// Assert
-		expect(component.find('.AppNameComponent__version').text()).toEqual(statusToSymbol.dev);
-		expect(component.find('.AppNameComponent__version').props().title).toEqual('v1.2.3-dev');
+			// Assert
+			expect(component.find('.AppNameComponent__version').text()).toEqual(statusToSymbol[status]);
+			expect(component.find('.AppNameComponent__version').parent().props().title).toEqual(`${APP_NAME} v1.2.3-${status}`);
+		});
 	});
 });
