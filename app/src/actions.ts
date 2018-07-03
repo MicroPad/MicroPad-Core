5fb82c6181d2814d2acdf837ea5da7d064cc1d9d
Nick Webster <nick@nick.geek.nz>
2018-07-03 20:49:43 +1200

    Bringing the NotepadDropdownContainer to v3

--- app/src/actions.ts
+++ app/src/actions.ts
@@ -1,5 +1,5 @@
 import actionCreatorFactory from 'redux-typescript-actions';
-import { INote, INotepad, IRenameNotepadObjectAction, NoteElement } from './types/NotepadTypes';
+import { IRenameNotepadObjectAction } from './types/NotepadTypes';
 import {
 	IAddToSyncAction,
 	IDeleteElementAction,
@@ -13,31 +13,33 @@ import {
 } from './types/ActionTypes';
 import { IInsertElementState } from './reducers/NoteReducer';
 import { AssetList, SyncedNotepadList, SyncLoginRequest, SyncUser } from './types/SyncTypes';
+import { FlatNotepad, Note, Notepad } from 'upad-parse/dist';
+import { NoteElement } from 'upad-parse/dist/Note';
 
 const actionCreator = actionCreatorFactory();
 
 export const actions = {
-	parseNpx: actionCreator.async<string, INotepad, any>('PARSE_NPX'),
-	saveNotepad: actionCreator.async<INotepad, void, any>('SAVE_NOTEPAD'),
+	parseNpx: actionCreator.async<string, Notepad, any>('PARSE_NPX'),
+	saveNotepad: actionCreator.async<Notepad, void, any>('SAVE_NOTEPAD'),
 	getNotepadList: actionCreator.async<void, string[], any>('GET_NOTEPAD_LIST'),
 	downloadNotepad: actionCreator.async<string, string, any>('DOWNLOAD_NOTEPAD'),
 	openNotepadFromStorage: actionCreator.async<string, void, any>('OPEN_NOTEPAD_FROM_STORAGE'),
 	renameNotepad: actionCreator.async<string, string, any>('RENAME_NOTEPAD'),
-	checkNoteAssets: actionCreator.async<[string, NoteElement[]], INotepad, any>('CHECK_NOTE_ASSETS'),
+	checkNoteAssets: actionCreator.async<[string, NoteElement[]], FlatNotepad, any>('CHECK_NOTE_ASSETS'),
 	loadNote: actionCreator.async<string, object, any>('LOAD_NOTE'),
 	downloadAsset: actionCreator.async<{ filename: string, uuid: string }, void, any>('DOWNLOAD_ASSET'),
 	expandAllExplorer: actionCreator.async<void, string[], any>('EXPAND_ALL_EXPLORER'),
 	print: actionCreator.async<void, NoteElement, void>('PRINT'),
 	syncLogin: actionCreator.async<SyncLoginRequest, SyncUser, any>('SYNC_LOGIN'),
 	getSyncedNotepadList: actionCreator.async<SyncUser, SyncedNotepadList, any>('SYNC_GET_NOTEPAD_LIST'),
-	syncDownload: actionCreator.async<string, INotepad, any>('SYNC_DOWNLOAD'),
+	syncDownload: actionCreator.async<string, Notepad, any>('SYNC_DOWNLOAD'),
 	syncUpload: actionCreator.async<ISyncAction, AssetList, any>('SYNC_UPLOAD'),
 	syncUploadAsset: actionCreator.async<IUploadAssetAction, void, any>('SYNC_UPLOAD_ASSET'),
 	deleteFromSync: actionCreator.async<string, void, any>('SYNC_DELETE'),
 	addToSync: actionCreator.async<IAddToSyncAction, string, any>('SYNC_CREATE'),
 
 	restoreJsonNotepad: actionCreator<string>('PARSE_JSON_NOTEPAD'),
-	newNotepad: actionCreator<INotepad>('NEW_NOTEPAD'),
+	newNotepad: actionCreator<FlatNotepad>('NEW_NOTEPAD'),
 	flipFullScreenState: actionCreator<void>('FLIP_FULL_SCREEN'),
 	deleteNotepad: actionCreator<string>('DELETE_NOTEPAD'),
 	exportNotepad: actionCreator<void>('EXPORT_NOTEPAD'),
@@ -46,10 +48,10 @@ export const actions = {
 	expandSection: actionCreator<string>('OPEN_SECTION'),
 	collapseSelection: actionCreator<string>('CLOSE_SECTION'),
 	search: actionCreator<string>('SEARCH'),
-	displayHashTagSearchResults: actionCreator<INote[]>('DISPLAY_HASH_TAG_SEARCH_RESULTS'),
+	displayHashTagSearchResults: actionCreator<Note[]>('DISPLAY_HASH_TAG_SEARCH_RESULTS'),
 	deleteNotepadObject: actionCreator<string>('DELETE_NOTEPAD_OBJECT'),
 	renameNotepadObject: actionCreator<IRenameNotepadObjectAction>('RENAME_NOTEPAD_OBJECT'),
-	expandFromNote: actionCreator<INote>('EXPAND_FROM_NOTE'),
+	expandFromNote: actionCreator<Note>('EXPAND_FROM_NOTE'),
 	collapseAllExplorer: actionCreator<void>('COLLAPSE_ALL_EXPLORER'),
 	openEditor: actionCreator<string>('OPEN_EDITOR'),
 	updateElement: actionCreator<IUpdateElementAction>('UPDATE_ELEMENT'),
