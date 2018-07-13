8eda26bd6e39230713f002c44907d8b0f0189f4d
Nick Webster <nick@nick.geek.nz>
2018-07-13 22:37:33 +1200

    Bumping version number

--- app/src/reducers/MetaReducer.ts
+++ app/src/reducers/MetaReducer.ts
@@ -10,7 +10,7 @@ export class MetaReducer implements IReducer<IMetaStoreState> {
 		version: {
 			major: 3,
 			minor: 7,
-			patch: 0,
+			patch: 1,
 			status: 'alpha'
 		},
 		isFullScreen: false,
