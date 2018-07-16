712b4132fc1f704f74213e176225d9c873586c24
Nick Webster <nick@nick.geek.nz>
2018-07-16 15:44:29 +1200

    Fixing hashtag result font colour

--- app/src/reducers/MetaReducer.ts
+++ app/src/reducers/MetaReducer.ts
@@ -9,8 +9,8 @@ export class MetaReducer implements IReducer<IMetaStoreState> {
 	public readonly initialState: IMetaStoreState = {
 		version: {
 			major: 3,
-			minor: 7,
-			patch: 2,
+			minor: 8,
+			patch: 0,
 			status: 'alpha'
 		},
 		isFullScreen: false,
