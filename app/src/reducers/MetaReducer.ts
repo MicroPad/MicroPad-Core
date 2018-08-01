394b95ae5dc7742d9821e91fa787b3907235bd36
Nick Webster <nick@nick.geek.nz>
2018-08-01 22:18:11 +1200

    Adjusting version number

--- app/src/reducers/MetaReducer.ts
+++ app/src/reducers/MetaReducer.ts
@@ -9,8 +9,8 @@ export class MetaReducer implements IReducer<IMetaStoreState> {
 	public readonly initialState: IMetaStoreState = {
 		version: {
 			major: 3,
-			minor: 8,
-			patch: 2,
+			minor: 9,
+			patch: 0,
 			status: 'beta'
 		},
 		isFullScreen: false,
