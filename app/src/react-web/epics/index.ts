fe1b1587b3e38162ac48ce1cf17b15569485dd57
Nick Webster <nick@nick.geek.nz>
2019-10-26 18:17:54 +1300

    Updating to follow the latest version  of redux-observable

--- app/src/react-web/epics/index.ts
+++ app/src/react-web/epics/index.ts
@@ -1,33 +1,9 @@
 // @ts-ignore
 import helpNpx from '!raw-loader!../assets/Help.npx';
 
-import { combineEpics, createEpicMiddleware } from 'redux-observable';
-import { notepadEpics$ } from './NotepadEpics';
-import { storageEpics$ } from './StorageEpics';
-import { noteEpics$ } from './NoteEpics';
-import { ExplorerEpics } from '../../core/epics/ExplorerEpics';
-import { AppEpics } from './AppEpics';
-import { PrintEpics } from './PrintEpics';
-import { SyncEpics } from './SyncEpics';
-import { HelpEpics } from './HelpEpics';
-import { SearchEpics } from '../../core/epics/SearchEpics';
+import { createEpicMiddleware } from 'redux-observable';
 import { getStorage } from '..';
-import { Action } from 'redux-typescript-actions';
-import { CryptoEpics } from './CryptoEpics';
 import { Dialog } from '../dialogs';
-
-const baseEpic$ = combineEpics(
-	notepadEpics$,
-	storageEpics$,
-	HelpEpics.helpEpics$,
-	SearchEpics.searchEpics$,
-	noteEpics$,
-	ExplorerEpics.explorerEpics$,
-	AppEpics.appEpics$,
-	PrintEpics.printEpics$,
-	SyncEpics.syncEpics$,
-	CryptoEpics.cryptoEpics$
-);
 
 export type EpicDeps = {
 	helpNpx: string,
@@ -35,10 +11,10 @@ export type EpicDeps = {
 	Dialog: Dialog
 };
 
-export const epicMiddleware = createEpicMiddleware<Action<any>, any, EpicDeps>(baseEpic$, {
+export const epicMiddleware = createEpicMiddleware({
 	dependencies: {
 		helpNpx,
-		getStorage: getStorage,
-		Dialog: Dialog
-	}
+		getStorage,
+		Dialog
+	} as EpicDeps
 });
