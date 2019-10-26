deee4b342d87abbac690e33e6ad932a3e5368fbd
Nick Webster <nick@nick.geek.nz>
2019-10-26 19:04:44 +1300

    Print works

--- app/src/react-web/epics/PrintEpics.ts
+++ app/src/react-web/epics/PrintEpics.ts
@@ -1,57 +1,60 @@
 import { combineEpics } from 'redux-observable';
-import { dataURItoBlob, isAction } from '../util';
+import { dataURItoBlob, filterTruthy, isAction } from '../util';
 import { actions } from '../../core/actions';
-import { filter, map, switchMap } from 'rxjs/operators';
+import { filter, map, switchMap, take, withLatestFrom } from 'rxjs/operators';
 import { INotepadStoreState } from '../../core/types/NotepadTypes';
 import { IStoreState } from '../../core/types';
 import { ASSET_STORAGE } from '..';
 import { trim } from '../components/note-viewer/elements/drawing/trim-canvas';
 import { Asset, FlatNotepad, Note } from 'upad-parse/dist';
 import { MarkdownNote, NoteElement } from 'upad-parse/dist/Note';
-import { from } from 'rxjs';
+import { from, Observable } from 'rxjs';
 
 export namespace PrintEpics {
-	export const generateMarkdownForPrint$ = (action$, store) =>
+	export const generateMarkdownForPrint$ = (action$, state$: Observable<IStoreState>) =>
 		action$.pipe(
 			isAction(actions.print.started),
-			map(() => store.getState()),
-			map((state: IStoreState) => [(state.notepads.notepad || <INotepadStoreState> {}).item, state.currentNote.ref]),
-			filter(([notepad, noteRef]: [FlatNotepad, string]) => !!notepad && !!noteRef),
-			map(([notepad, noteRef]: [FlatNotepad, string]) => notepad.notes[noteRef]),
-			filter(Boolean),
-			switchMap((note: Note) =>
-				from((async () => {
-					const resolvedAssets = await Promise.all(note.elements
-						.filter(e => e.type === 'drawing' || e.type === 'image')
-						.filter(e => !!e.args.ext)
-						.map(async e => {
-							const blob: Blob = <Blob> await ASSET_STORAGE.getItem(e.args.ext!);
-							const data = (e.type === 'drawing') ? dataURItoBlob(await getTrimmedDrawing(blob)) : blob;
-							return { uuid: e.args.ext!, data };
-						}));
+			switchMap(() => state$.pipe(
+				take(1),
+				map((state: IStoreState) => [(state.notepads.notepad || <INotepadStoreState> {}).item, state.currentNote.ref]),
+				filter(([notepad, noteRef]: [FlatNotepad, string]) => !!notepad && !!noteRef),
+				map(([notepad, noteRef]: [FlatNotepad, string]) => notepad.notes[noteRef]),
+				filterTruthy(),
+				switchMap((note: Note) =>
+					from((async () => {
+						const resolvedAssets = await Promise.all(note.elements
+							.filter(e => e.type === 'drawing' || e.type === 'image')
+							.filter(e => !!e.args.ext)
+							.map(async e => {
+								const blob: Blob = <Blob> await ASSET_STORAGE.getItem(e.args.ext!);
+								const data = (e.type === 'drawing') ? dataURItoBlob(await getTrimmedDrawing(blob)) : blob;
+								return { uuid: e.args.ext!, data };
+							}));
 
-					const assets: Asset[] = resolvedAssets.map(res => new Asset(res.data, res.uuid));
+						const assets: Asset[] = resolvedAssets.map(res => new Asset(res.data, res.uuid));
 
-					return [
-						note,
-						assets
-					];
-				})())
-			),
-			switchMap(([note, assets]: [Note, Asset[]]) => from((note as Note).toMarkdown(assets))),
-			map((mdNote: MarkdownNote) => <NoteElement> {
-				content: mdNote.md,
-				args: {
-					id: 'markdown1_print',
-					width: 'auto',
-					height: 'auto',
-					fontSize: (<IStoreState> store.getState()).app.defaultFontSize,
-					x: '0px',
-					y: '0px'
-				},
-				type: 'markdown'
-			}),
-			map((element: NoteElement) => actions.print.done({ params: undefined, result: element }))
+						return [
+							note,
+							assets
+						];
+					})())
+				),
+				switchMap(([note, assets]: [Note, Asset[]]) => from((note as Note).toMarkdown(assets))),
+				withLatestFrom(state$),
+				map(([mdNote, state]: [MarkdownNote, IStoreState]) => <NoteElement> {
+					content: mdNote.md,
+					args: {
+						id: 'markdown1_print',
+						width: 'auto',
+						height: 'auto',
+						fontSize: state.app.defaultFontSize,
+						x: '0px',
+						y: '0px'
+					},
+					type: 'markdown'
+				}),
+				map((element: NoteElement) => actions.print.done({ params: undefined, result: element }))
+			))
 		);
 
 	export const printEpics$ = combineEpics(
