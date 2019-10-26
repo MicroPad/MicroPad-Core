e5d9f64660bcec66ac37230a751e7aa2a6d78d5c
Nick Webster <nick@nick.geek.nz>
2019-10-26 18:17:28 +1300

    Search epics

--- app/src/core/epics/SearchEpics.ts
+++ app/src/core/epics/SearchEpics.ts
@@ -1,14 +1,10 @@
-// @ts-ignore
-
 import { combineEpics } from 'redux-observable';
-import { catchError, filter, map, switchMap } from 'rxjs/operators';
+import { catchError, filter, map, switchMap, take, withLatestFrom } from 'rxjs/operators';
 import { from, Observable, of } from 'rxjs';
 import { Action, isType } from 'redux-typescript-actions';
 import { actions } from '../actions';
 import { HashTagSearchResult, HashTagSearchResults } from '../reducers/SearchReducer';
 import { IStoreState } from '../types';
-import { Store } from 'redux';
-import { SearchIndices } from '../types/ActionTypes';
 import { isAction } from '../../react-web/util';
 import { indexNotepads } from '../../react-web/SearchWorker';
 
@@ -19,28 +15,35 @@ export namespace SearchEpics {
 			map(() => actions.indexNotepads.started(undefined))
 		);
 
-	export const indexNotepads$ = (action$, store: Store<IStoreState>) =>
+	export const indexNotepads$ = (action$, state$: Observable<IStoreState>) =>
 		action$.pipe(
 			isAction(actions.indexNotepads.started),
-			map(() => store.getState().search.indices),
-			switchMap((indices: SearchIndices) =>
-				from(indexNotepads(indices, store.getState().notepadPasskeys)).pipe(
-					map(newIndices => actions.indexNotepads.done({ params: undefined, result: newIndices })),
-					catchError(err => of(actions.indexNotepads.failed({ params: undefined, error: err })))
+			switchMap(() => state$.pipe(
+				take(1),
+				map(state => ({
+					indices: state.search.indices,
+					notepadPasskeys: state.notepadPasskeys
+				})),
+				switchMap(({ indices, notepadPasskeys }) =>
+					from(indexNotepads(indices, notepadPasskeys)).pipe(
+						map(newIndices => actions.indexNotepads.done({ params: undefined, result: newIndices })),
+						catchError(err => of(actions.indexNotepads.failed({ params: undefined, error: err })))
+					)
 				)
-			)
+			))
 		);
 
-	export const search$ = (action$, store: Store<IStoreState>) =>
+	export const search$ = (action$, state$: Observable<IStoreState>) =>
 		action$.pipe(
 			filter((action: Action<string>) => isType(action, actions.search)),
 			map((action: Action<string>) => action.payload),
-			switchMap((query: string) => from((async () => {
+			withLatestFrom(state$),
+			switchMap(([query, state]: [string, IStoreState]) => from((async () => {
 				if (query.length <= 1 || query.substring(0, 1) !== '#') return actions.displayHashTagSearchResults({});
 
 				// Create a data structure with each notepad being the key to all the results for that hashtag's search
 				const results: HashTagSearchResults = {};
-				store.getState().search.indices
+				state.search.indices
 					.forEach(index =>
 						index.notepad.search(index.trie, query)
 							.map(note => {
