2318ce73352584f4d94b4faf3b0b19e2c9a60acb
Nick Webster <nick@nick.geek.nz>
2019-10-26 18:26:27 +1300

    Help epics are sorted

--- app/src/react-web/epics/HelpEpics.ts
+++ app/src/react-web/epics/HelpEpics.ts
@@ -1,36 +1,38 @@
 import { combineEpics } from 'redux-observable';
-import { concatMap, filter, map } from 'rxjs/operators';
+import { concatMap, map, switchMap, take } from 'rxjs/operators';
 import { Action, Success } from 'redux-typescript-actions';
 import { actions } from '../../core/actions';
 import { from, Observable } from 'rxjs';
 import { IStoreState } from '../../core/types';
 import { Store } from 'redux';
 import { EpicDeps } from './index';
-import { isAction } from '../util';
+import { filterTruthy, isAction } from '../util';
 
 export namespace HelpEpics {
-	export const getHelp$ = (action$: Observable<Action<void>>, store: Store<IStoreState>, { Dialog }) =>
+	export const getHelp$ = (action$: Observable<Action<void>>, state$: Observable<IStoreState>, { Dialog }: EpicDeps) =>
 		action$.pipe(
 			isAction(actions.getHelp.started),
-			concatMap(() =>
+			switchMap(() => state$.pipe(take(1))),
+			concatMap(state =>
 				from((async () => {
-					const notepadList = store.getState().notepads.savedNotepadTitles;
+					const notepadList = state.notepads.savedNotepadTitles;
 					if (!notepadList || !notepadList.includes('Help')) return true;
 
-					return await Dialog.confirm(`You have already imported the Help notebook. It can be accessed from the notebooks dropdown. If you continue you will lose any changes made to the notebook.`);
+					// @ts-ignore, typescript is incorrect about this
+					return Dialog.confirm(`You have already imported the Help notebook. It can be accessed from the notebooks dropdown. If you continue you will lose any changes made to the notebook.`);
 				})())
 			),
-			filter(Boolean),
+			filterTruthy(),
 			map(() => actions.getHelp.done({ params: undefined, result: undefined }))
 		);
 
-	export const getHelpSuccess$ = (action$: Observable<Action<Success<void, void>>>, store: Store<IStoreState>, { helpNpx }) =>
+	export const getHelpSuccess$ = (action$: Observable<Action<Success<void, void>>>, _, { helpNpx }) =>
 		action$.pipe(
 			isAction(actions.getHelp.done),
 			map(() => actions.parseNpx.started(helpNpx))
 		);
 
-	export const helpEpics$ = combineEpics<Action<any>, any, EpicDeps>(
+	export const helpEpics$ = combineEpics(
 		getHelp$,
 		getHelpSuccess$
 	);
