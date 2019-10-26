ad7c90b2cf0959e0c53a2af2d18911ca86bb0470
Nick Webster <nick@nick.geek.nz>
2019-10-26 18:24:58 +1300

    Crypto epics are good

--- app/src/react-web/epics/CryptoEpics.ts
+++ app/src/react-web/epics/CryptoEpics.ts
@@ -3,20 +3,22 @@ import { Action } from 'redux-typescript-actions';
 import { elvis, isAction, resolveElvis } from '../util';
 import { actions } from '../../core/actions';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, switchMap, take } from 'rxjs/operators';
 import { AddCryptoPasskeyAction } from '../../core/types/ActionTypes';
 import { IStoreState } from '../../core/types';
-import { Store } from 'redux';
 
 export namespace CryptoEpics {
-	export const encryptNotepad$ = (action$: Observable<Action<string>>, store: Store<IStoreState>) =>
+	export const encryptNotepad$ = (action$: Observable<Action<string>>, state$: Observable<IStoreState>) =>
 		action$.pipe(
 			isAction(actions.encryptNotepad),
-			map(action => ({
-				passkey: action.payload,
-				notepadTitle: resolveElvis(elvis(store.getState().notepads).notepad.item.title)
-			} as AddCryptoPasskeyAction)),
-			map(payload => actions.addCryptoPasskey(payload))
+			switchMap(action => state$.pipe(
+				take(1),
+				map(state => ({
+					passkey: action.payload,
+					notepadTitle: resolveElvis(elvis(state.notepads).notepad.item.title)
+				} as AddCryptoPasskeyAction)),
+				map(payload => actions.addCryptoPasskey(payload))
+			))
 		);
 
 	export const cryptoEpics$ = combineEpics(
