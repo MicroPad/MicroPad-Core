34c0c25b59edbc703ec19c32a177e6bc75a3bec1
Nick Webster <nick@nick.geek.nz>
2021-10-31 23:48:30 +1300

    Run the entire app in the persistent context

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,18 +2,31 @@ import { isDev } from './app/util';
 import * as serviceWorker from './registerServiceWorker';
 import { initWasm } from './app/init-wasm';
 
-// `window.isSupported` is set by the Unsupported Browser logic.
-if (window.isSupported) {
-	// If you want your app to work offline and load faster, you can change
-	// unregister() to register() below. Note this comes with some pitfalls.
-	// Learn more about service workers: https://bit.ly/CRA-PWA
-	if (window.isElectron || isDev(false)) {
-		serviceWorker.unregister();
-	} else {
-		serviceWorker.register();
-	}
+if (navigator.storage && navigator.storage.persist) {
+	navigator.storage.persist().then(storageAllowed => {
+		if (!storageAllowed) {
+			alert(`Failed to get permission for long-term storage. Notebooks may be removed from your devices storage under storage pressure.`);
+		}
+		initMicroPad();
+	});
+} else {
+	initMicroPad();
+}
 
-	initWasm().then(() => import('./app/root'));
+function initMicroPad() {
+	// `window.isSupported` is set by the Unsupported Browser logic.
+	if (window.isSupported) {
+		// If you want your app to work offline and load faster, you can change
+		// unregister() to register() below. Note this comes with some pitfalls.
+		// Learn more about service workers: https://bit.ly/CRA-PWA
+		if (window.isElectron || isDev(false)) {
+			serviceWorker.unregister();
+		} else {
+			serviceWorker.register();
+		}
+
+		initWasm().then(() => import('./app/root'));
+	}
 }
 
 export {};
