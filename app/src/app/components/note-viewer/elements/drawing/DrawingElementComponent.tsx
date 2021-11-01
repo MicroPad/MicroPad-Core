8ea7f21002e7e3127a6c5e2e475ea005a2dc2456
Jonas-July <jonas.july@outlook.de>
2021-11-01 13:40:11 +0100

    Try input type=color for colour selection
    drawColour is changed but icon doesn't change colour

--- app/src/app/components/note-viewer/elements/drawing/DrawingElementComponent.tsx
+++ app/src/app/components/note-viewer/elements/drawing/DrawingElementComponent.tsx
@@ -52,6 +52,9 @@ export default class DrawingElementComponent extends React.Component<IDrawingEle
 	private rainbowIndex = 0;
 
 	private setDrawColour = (e, colour) => {
+		console.log(e.target);
+		console.log(e.target.value);
+		console.log(colour);
 		this.drawColour = colour;
 		this.isErasing = false;
 		this.isRainbow = false;
@@ -59,11 +62,11 @@ export default class DrawingElementComponent extends React.Component<IDrawingEle
 		// Complete hack, but I don't know how to actually implement radio buttons
 		// Also doesn't really work when clicking on colours multiple times
 		// but it's enough to demo the idea
-		var buttons = Array.from((e.target as HTMLInputElement).parentNode.parentNode.parentNode.children);
+		/*var buttons = Array.from((e.target as HTMLInputElement).parentNode.parentNode.parentNode.children);
 		buttons.forEach(b => b.firstChild.firstChild.checked = false);
 		(e.target as HTMLInputElement).checked = true;
 		console.log((e.target as HTMLInputElement).checked);
-
+		//*/
 	}
 
 	render() {
@@ -141,6 +144,9 @@ export default class DrawingElementComponent extends React.Component<IDrawingEle
 						</Col>
 						<Col>
 							<Checkbox label="Purple" value="1" checked={this.drawColour === colours.PURPLE} filledIn onChange={e => this.setDrawColour(e, colours.PURPLE)} />
+						</Col>
+						<Col>
+							<input type="color" value={this.drawColour} onChange={e => this.setDrawColour(e, e.target.value)} />
 						</Col>
 					</Row>
 
