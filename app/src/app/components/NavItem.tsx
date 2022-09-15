fb684f3b8aec2d86a5777ea5f8378cab34299b0a
Nick Webster <nick@nick.geek.nz>
2022-09-15 15:34:52 +1200

    Fixing drag click issue

--- app/src/app/components/NavItem.tsx
+++ app/src/app/components/NavItem.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavItem, NavItemProps } from 'react-materialize';
 import { isIOS } from '../services/BrowserDetection';
 
 const IS_IOS = isIOS();
 
-const NavItem2 = (props: NavItemProps) => {
+const NavItem2 = React.memo((props: NavItemProps) => {
+	const [isDragging, setIsDragging] = useState(false);
+
+	const onTouchStart = IS_IOS ? () => setIsDragging(false) : undefined;
+	const onTouchMove = IS_IOS ? () => setIsDragging(true) : undefined;
+	const onTouchEnd = isDragging ? undefined : props.onClick;
+
 	// @ts-expect-error Adding an extra property that's not on the type (onTouchEnd). The real JS uses it.
-	return <NavItem {...props} onClick={IS_IOS ? undefined : props.onClick} onTouchEnd={IS_IOS ? props.onClick : undefined} />;
-}
+	return <NavItem {...props} onClick={IS_IOS ? undefined : props.onClick} onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={IS_IOS ? onTouchEnd : undefined} />;
+});
 
 export default NavItem2;
