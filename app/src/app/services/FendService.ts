d7891e3e944a0f9b1a57b573174a80b08d8bff74
Nick Webster <nick@nick.geek.nz>
2022-10-30 15:39:28 -0700

    Add currency support in fend (note: a fend release needs to happen before this can be used)

---
+++ app/src/app/services/FendService.ts
@@ -0,0 +1,31 @@
+import { Observable, of, shareReplay } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { catchError, map } from 'rxjs/operators';
+import { initialise, initialiseWithHandlers } from '../../../../../../dolly/fend/wasm/pkgweb';
+
+export const START_FEND$: Observable<void> = ajax<any>({
+	url: 'https://getmicropad.com/cors-proxy/moolah.xml',
+	method: 'GET',
+	timeout: 3000,
+	responseType: 'text'
+}).pipe(
+	map(xml => {
+		const raw = new DOMParser().parseFromString(xml.response, 'application/xml');
+		const currencies = new Map<string, number>();
+		raw.querySelectorAll('Cube > Cube > Cube').forEach(el => {
+			const currency = el.getAttribute('currency');
+			const rate = el.getAttribute('rate');
+			if (!currency || !rate) throw new Error(`Unexpected currency data format`);
+			const rateF = parseFloat(rate);
+			if (isNaN(rateF)) throw new Error(`Currency rate (${rate}) for ${currency} was NaN`);
+			currencies.set(currency, rateF);
+		});
+		return currencies;
+	}),
+	map(currencyData => initialiseWithHandlers(currencyData)),
+	catchError(err => {
+		console.error('Caught an error getting currencies for fend', err);
+		return of(initialise());
+	}),
+	shareReplay(1)
+);
